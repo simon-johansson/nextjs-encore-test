c8245d57a667bb7251520cedf6ae6cc9b7bb2286
Simon Johansson <simon@encore.dev>
2024-04-25 16:14:41 +0200

    Init

---
+++ backend/encore.app
@@ -0,0 +1,4 @@
+{
+  "id": "next-js-test-ts-9wvi",
+  "experiments": ["typescript"]
+}
---
+++ frontend/app/admin/page.tsx
@@ -0,0 +1,26 @@
+import { redirect } from "next/navigation";
+import getRequestClient from "../lib/getRequestClient";
+import { admin, APIError, ErrCode } from "../lib/client";
+
+export default async function Admin() {
+  const client = getRequestClient();
+  let response: admin.DashboardData;
+  try {
+    response = await client.admin.getDashboardData();
+  } catch (e) {
+    console.error(e);
+    const error = e as APIError;
+    if (error.code === ErrCode.Unauthenticated) {
+      return redirect("/auth/unauthenticated?from=%2Fadmin");
+    }
+    throw error;
+  }
+
+  return (
+    <section>
+      <h1 className="text-3xl">Admin Dashboard</h1>
+      <br />
+      <p>{response.value}</p>
+    </section>
+  );
+}
